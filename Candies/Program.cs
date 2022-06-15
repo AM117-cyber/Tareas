21ea393775774293feb8eb87224cdebf826c4aea
Melissa <anamelissa1707@gmail.com>
2022-06-14 22:44:47 -0700

    DUDA

---
+++ Candies/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ingrese el numero de caramelos por caja con un espacio entre las cantidades.");
+//DUDA: Cuando pongo 12 4 5 me da error.
+string candies =  Console.ReadLine();
+string[] numbers = candies.Split(" ");
+int[] candiesForMethod = new int[numbers.Length];
+for (int i = 0; i < numbers.Length; i++)
+{
+    candiesForMethod[i] = Int32.Parse(numbers[i]);
+}
+int min = int.MaxValue;
+for (int i = 0; i < numbers.Length; i++)
+{
+    min = Math.Min(candiesForMethod[i],min);
+}
+int difference = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    difference += candiesForMethod[i]-min;
+}
+System.Console.WriteLine(difference);
