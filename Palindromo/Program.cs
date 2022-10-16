6516ec59e5666d52cd2ebba2ba96c1f7dde8b0b2
Melissa <anamelissa1707@gmail.com>
2022-10-15 17:52:05 -0700

    fixing insertions

--- Palindromo/Program.cs
+++ Palindromo/Program.cs
@@ -1,21 +1,22 @@
 ﻿static void IsPalyndrome1(string word)
 {
 
-    Console.WriteLine(IsPalyndrome(word, 0, word.Length-1,true,0));
+    Console.WriteLine(IsPalyndrome(word, 0, word.Length-1,0).ToString());
 }
-IsPalyndrome1("cafsecaseac");
-static (bool b, int insertions) IsPalyndrome(string word,int pos1, int pos2,bool b, int insertions)
+IsPalyndrome1("caseccasac");
+static int IsPalyndrome(string word,int pos1, int pos2, int insertions)
 {   
     if (pos1 >= pos2)
     {
-        return (b,insertions);
+        return insertions;
     }
     if (word[pos1] != word[pos2])
     {
-        b = false;
-        insertions = insertions+2; 
+        
+        return Math.Min(IsPalyndrome(word, pos1+1,pos2,insertions+1),IsPalyndrome(word, pos1,pos2-1,insertions+1));
+        
     }
     
-    return IsPalyndrome(word, pos1+1,pos2-1,b,insertions);
+    return IsPalyndrome(word, pos1+1,pos2-1,insertions);
 }
 
